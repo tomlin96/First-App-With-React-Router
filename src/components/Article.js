3170cce806c9fbc0fecfbe6bec851db639419939
Tomasz Lichota <tomaszlichota.96@gmail.com>
2021-01-11 21:06:34 +0100

    hange Article.js, App.css, Footer.css

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,26 +1,51 @@
 import React from 'react';
 
-const Article = ({ title, author, text }) => {
+const Article = ({ title, author, text, id }) => {
 
-    const styles = {
-        marginTop: 40
+
+    if (id != 1) {
+        const styles = {
+            marginTop: 40
+        }
+        return (
+            <article style={styles}>
+                <h3 style={{
+                    marginBottom: 3,
+                    textTransform: "uppercase",
+                }}>{title}</h3>
+                <span style={{
+                    display: "block",
+                    marginBottom: 10,
+                    fontSize: 12,
+                }}>{author}</span>
+                <p style={{
+                    fontSize: 15,
+                }}>{text}</p>
+            </article>
+        );
+    } else {
+        const styles = {
+            marginTop: 0
+        }
+        return (
+            <article style={styles}>
+                <h3 style={{
+                    marginBottom: 3,
+                    textTransform: "uppercase",
+                }}>{title}</h3>
+                <span style={{
+                    display: "block",
+                    marginBottom: 10,
+                    fontSize: 12,
+                }}>{author}</span>
+                <p style={{
+                    fontSize: 15,
+                }}>{text}</p>
+            </article>
+        );
     }
-    return (
-        <article style={styles}>
-            <h3 style={{
-                marginBottom: 3,
-                textTransform: "uppercase",
-            }}>{title}</h3>
-            <span style={{
-                display: "block",
-                marginBottom: 10,
-                fontSize: 12,
-            }}>{author}</span>
-            <p style={{
-                fontSize: 15,
-            }}>{text}</p>
-        </article>
-    );
+
+
 }
 
 export default Article;
