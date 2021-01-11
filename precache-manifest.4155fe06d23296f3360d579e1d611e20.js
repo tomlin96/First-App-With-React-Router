a349531766e57896a1ec0b259e6a0f0192d5c85e
Tomasz Lichota <tomaszlichota.96@gmail.com>
2021-01-11 13:34:11 +0100

    Updates

---
+++ precache-manifest.4155fe06d23296f3360d579e1d611e20.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e4a296661c0f036a8cc",
+    "url": "/Page-Example/static/css/main.54952d03.chunk.css"
+  },
+  {
+    "revision": "0e4a296661c0f036a8cc",
+    "url": "/Page-Example/static/js/main.cead4d56.chunk.js"
+  },
+  {
+    "revision": "196187a93339c56fdb14",
+    "url": "/Page-Example/static/js/runtime~main.da9fc226.js"
+  },
+  {
+    "revision": "2bb4f6e29227df83a10f",
+    "url": "/Page-Example/static/js/2.c68aea12.chunk.js"
+  },
+  {
+    "revision": "6f8aa3a9d844090f7ae513dd56fb2d59",
+    "url": "/Page-Example/static/media/header2.6f8aa3a9.jpg"
+  },
+  {
+    "revision": "9facf55a24038dd374eec9a6bfad44ac",
+    "url": "/Page-Example/static/media/header1.9facf55a.jpg"
+  },
+  {
+    "revision": "bfb8b51cd632675ef02a99860b7da254",
+    "url": "/Page-Example/static/media/header3.bfb8b51c.jpg"
+  },
+  {
+    "revision": "e5a00a6fc7845d76e0d0f392359b6e11",
+    "url": "/Page-Example/static/media/header4.e5a00a6f.jpg"
+  },
+  {
+    "revision": "68feff82cf8fe336df3ebf512f2b90a3",
+    "url": "/Page-Example/index.html"
+  }
+];
