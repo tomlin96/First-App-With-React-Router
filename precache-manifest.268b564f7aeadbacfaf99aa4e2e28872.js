57f6df6266b245e8b13a63669f01a76101c94634
Tomasz Lichota <tomaszlichota.96@gmail.com>
2021-01-13 10:31:19 +0100

    Updates

---
+++ precache-manifest.268b564f7aeadbacfaf99aa4e2e28872.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc6d783c57fbc8e58db2",
+    "url": "/First-App-With-React-Router/static/css/main.51258a34.chunk.css"
+  },
+  {
+    "revision": "cc6d783c57fbc8e58db2",
+    "url": "/First-App-With-React-Router/static/js/main.fd25a244.chunk.js"
+  },
+  {
+    "revision": "d073d99c0403326d12c8",
+    "url": "/First-App-With-React-Router/static/js/runtime~main.64b87a22.js"
+  },
+  {
+    "revision": "2bb4f6e29227df83a10f",
+    "url": "/First-App-With-React-Router/static/js/2.c68aea12.chunk.js"
+  },
+  {
+    "revision": "6f8aa3a9d844090f7ae513dd56fb2d59",
+    "url": "/First-App-With-React-Router/static/media/header2.6f8aa3a9.jpg"
+  },
+  {
+    "revision": "9facf55a24038dd374eec9a6bfad44ac",
+    "url": "/First-App-With-React-Router/static/media/header1.9facf55a.jpg"
+  },
+  {
+    "revision": "bfb8b51cd632675ef02a99860b7da254",
+    "url": "/First-App-With-React-Router/static/media/header3.bfb8b51c.jpg"
+  },
+  {
+    "revision": "e5a00a6fc7845d76e0d0f392359b6e11",
+    "url": "/First-App-With-React-Router/static/media/header4.e5a00a6f.jpg"
+  },
+  {
+    "revision": "bf91f46149745511cceacc61fd33adfb",
+    "url": "/First-App-With-React-Router/index.html"
+  }
+];
